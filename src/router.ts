8d105d1c4b147c3306b654aaeababadcd9c229c9
daniilmiagkov <d.miagkov@gmail.com>
2025-08-12 00:39:13 +0700

    remove router

--- src/router.ts
+++ src/router.ts
@@ -9,23 +9,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(), // <-- здесь
   routes,
-  scrollBehavior(to, from) {
-    // Если переход осуществляется с использованием router.replace и содержит хэш
-    if (to.hash && from.path === to.path) {
-      // Не выполняем прокрутку
-      return false
-    }
-
-    // Обычное поведение прокрутки
-    if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth',
-      }
-    }
-
-    return { top: 0 }
-  },
 })
 
 export default router
