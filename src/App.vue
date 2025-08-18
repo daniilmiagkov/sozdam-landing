41fc47ed6c6e71f52e8daae7559d6d34d6bc8bb4
daniilmiagkov <d.miagkov@gmail.com>
2025-08-18 13:29:38 +0300

    remove hashes

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,12 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
 import Header from './components/Header.vue'
-import ArchitectureSection from './components/sections/Architecture.vue'
-import DatasetSection from './components/sections/Dataset.vue'
-import DemoSection from './components/sections/Demo.vue'
-import RelevanceSection from './components/sections/Relevance.vue'
-import TeamSection from './components/sections/Team.vue'
-
-const route = useRoute()
-const router = useRouter()
-
-const sections = ['relevance', 'architecture', 'dataset', 'team', 'demo']
-
-const currentSection = ref('relevance')
-
-// Функция установки секции и корректного хэша
-function updateSection(hash: string) {
-  const cleanHash = hash.replace('#', '')
-  if (sections.includes(cleanHash)) {
-    currentSection.value = cleanHash
-  }
-  else {
-    currentSection.value = 'relevance'
-    // обновляем URL без перезагрузки страницы
-    router.replace({ path: route.path, hash: '#relevance' })
-  }
-}
-
-watch(() => route.hash, (newHash) => {
-  updateSection(newHash)
-})
-
-onMounted(() => {
-  updateSection(route.hash)
-})
 </script>
 
 <template>
   <div :class="$style.app">
     <Header />
     <main :class="$style.main">
-      <RelevanceSection v-show="currentSection === 'relevance'" />
-      <ArchitectureSection v-show="currentSection === 'architecture'" />
-      <DatasetSection v-show="currentSection === 'dataset'" />
-      <TeamSection v-show="currentSection === 'team'" />
-      <DemoSection v-show="currentSection === 'demo'" />
+      <router-view />
     </main>
   </div>
 </template>
--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
+import AppLayout from './App.vue'
+import Gsap from './components/Gsap.vue'
+import ArchitectureSection from './components/sections/Architecture.vue'
+import DatasetSection from './components/sections/Dataset.vue'
+import DemoSection from './components/sections/Demo.vue'
+import RelevanceSection from './components/sections/Relevance.vue'
+import TeamSection from './components/sections/Team.vue'
 
 const routes = [
-  { path: '/', component: App },
-  { path: '/:catchAll(.*)', component: App }, // любой неизвестный путь ведёт на App
+  {
+    path: '/',
+    component: AppLayout,
+    children: [
+      { path: '', redirect: '/relevance' },
+      { path: 'relevance', component: RelevanceSection },
+      { path: 'architecture', component: ArchitectureSection },
+      { path: 'dataset', component: DatasetSection },
+      { path: 'team', component: TeamSection },
+      { path: 'demo', component: DemoSection },
+    ],
+  },
+  { path: '/gsap', component: Gsap },
+
+  { path: '/:catchAll(.*)', redirect: '/relevance' },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from) {
-    if (to.hash && from.path === to.path)
-      return false
-    if (to.hash)
-      return { el: to.hash, behavior: 'smooth' }
-    return { top: 0 }
-  },
-})
-
-// Всегда устанавливаем #relevance, если хэш пустой
-router.beforeEach((to, _, next) => {
-  if (!to.hash) {
-    next({ path: to.path, hash: '#relevance' })
-  }
-  else {
-    next()
-  }
 })
 
 export default router
